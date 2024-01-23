b7527d8424e8733114571b80950c76e78decb9ea
Emmett-Kogan <emmett.kogan@gmail.com>
2024-01-23 10:45:22 -0500

    Made submission directory and finished readme/basic testing. Want to fix the script I tried making so it actually works though.

---
+++ 1/src/35410250/README.md
@@ -0,0 +1,6 @@
+# Assignment 1
+## Emmett Kogan
+
+### Building and running
+1. Run `make`, the `default` rule produces each executable files `mapper`, `reducer` and `combiner`. The `clean` rule deletes them.
+2. The programs can now be tested with the same commands as provided in the assignment document
---
+++ 1/src/35410250/wrline.h
@@ -0,0 +1,21 @@
+#pragma once
+
+int readline(char *buffer, int max) {
+	int count = 0;
+	while(--max) {
+		count++;
+		read(0, buffer, 1);
+		if (*(buffer++) == '\n') {
+			*buffer = '\0';
+			return count;
+		}
+	}
+
+	return count;
+}
+
+int writeline(int pfd, char *buffer) {
+	int i = 0;
+	while (buffer[i]) write(pfd, buffer+(i++), 1);
+	return i;
+}
