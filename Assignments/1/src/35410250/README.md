446cca394d19c8831100fda8738bdb3e1329e251
Emmett-Kogan <emmett.kogan@gmail.com>
2024-01-31 10:37:53 -0500

    Removed extra pipe from assignment 1. I have no clue why I used 2 pipes originally now

--- Assignments/1/src/35410250/README.md
+++
@@ -1,6 +0,0 @@
-# Assignment 1
-## Emmett Kogan
-
-### Building and running
-1. Run `make`, the `default` rule produces each executable files `mapper`, `reducer` and `combiner`. The `clean` rule deletes them.
-2. The programs can now be tested with the same commands as provided in the assignment document
--- Assignments/1/src/combiner.c
+++ Assignments/1/src/combiner.c
@@ -9,17 +9,11 @@
 
 int main() {
     int pid_mapper, pid_reducer, return_status;
-    int pfd1[2], pfd2[2];
+    int pfd[2];
     char buffer[32];
 
     // Setup pipes
-    if (pipe(pfd1) == -1) perror("Pipe 1");
-    if (pipe(pfd2) == -1) perror("Pipe 2");
-
-    // Note that pipe one is written to by combiner and read by mapper
-    // And pipe two is written to by mapper and read by reducer
-
-    // Where mapper's fd 0 and 1 are pipes, and reducer's fd 0 is a pipe
+    if (pipe(pfd) == -1) perror("Pipe");
 
     // Mapper
     pid_mapper = fork();
@@ -28,12 +22,10 @@ int main() {
             perror("fork 1");
         case 0:
             // Need to close write end of pipe 1 and read end of pipe 2
-            close(pfd1[1]);
-            close(pfd2[0]);
+            close(pfd[0]);
 
-            // Replace std in and out with the pipes
-            dup2(pfd1[0], 0);
-            dup2(pfd2[1], 1);
+            // Replace std out with a pipe
+            dup2(pfd[1], 1);
 
             // Run the mapper program and kill the child process
             execl("mapper", "mapper", NULL);
@@ -47,13 +39,11 @@ int main() {
         case -1:
             perror("fork 2");
         case 0:
-            // Need to close both ends of pipe 1 and the write end of pipe 2
-            close(pfd1[0]);
-            close(pfd1[1]);
-            close(pfd2[1]);
+            // Need to close write end of pipe
+            close(pfd[1]);
 
             // Replace std in with read end of pipe 2
-            dup2(pfd2[0], 0);
+            dup2(pfd[0], 0);
 
             // Run the reducer program and kill the child process
             execl("reducer", "reducer", NULL);
@@ -62,20 +52,8 @@ int main() {
     }
 
     // Parent
-
-    // Close unused ends of pipes
-    close(pfd1[0]);
-    close(pfd2[0]);
-    close(pfd2[1]);
-
-    // Take all input from stdin and write it to pipe 1 (to mapper)
-    while(1) {
-        if (readline(buffer, 24) == 0 || !buffer[0] || buffer[0] == '\n')
-            break;
-        writeline(pfd1[1], buffer);
-        memset(buffer,0,32);
-    }
-    write(pfd1[1], "\n", 1);
+    close(pfd[0]);
+    close(pfd[1]);
 
     while(wait(NULL) > 0);
 
