a99914f706efa77c7ef19670594e37a3d0d29a94
Emmett-Kogan <emmett.kogan@gmail.com>
2024-02-04 02:30:57 -0500

    Update append.sh

--- append.sh
+++ append.sh
@@ -2,8 +2,6 @@
 # Provide a relative path to the target directory in arg1 and the date in arg2
 
 for file in $1/*; do
-	echo "$file"
-
 	if [[ "$file" == *.c || "$file" == *.h || "$file" == *.cpp ]]; then
 		echo "// Author: Emmett Kogan" >> t.txt
 		echo "// Last modified: $2" >> t.txt
