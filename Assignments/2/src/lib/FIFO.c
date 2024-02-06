605388f71dc2a846ce663c2be0742ebe888aeec7
Emmett-Kogan <emmett.kogan@gmail.com>
2024-02-06 01:25:40 -0500

    Update FIFO.c
    Added comment about maybe using a conditional variable instead of a counting semaphore, I think it would reduce the amount of mutexes I use but at the same time I think I would also lose the ability to do reads and writes at the same time using this structure. Not sure if one approach is better than the other yet or even if my current implementation convers all of the cases.

--- Assignments/2/src/lib/FIFO.c
+++ Assignments/2/src/lib/FIFO.c
@@ -22,6 +22,18 @@ int FIFO_init(FIFO_t *f, uint32_t depth, uint32_t width) {
     return 0;
 }
 
+/*
+ * Hypothetically if I wanted to do this with a condition variable, I could do one like in the examples
+ * execpt instead of being directly related to count, like with the counting semaphore approach, it would
+ * be related indirectly via a full/empty flag I guess that would be the condition, and a mutex. If I want
+ * both writes and reads to be done one at a time to prevent race conditions, there would need to be 3 mutexes
+ * total this way, one for read, one for write, and one for full/empty. So, if a thread picks up for instance the write lock,
+ * and then tries to obtain the full/empty lock and gets blocked, then it would have to wait on whatever is holding it, 
+ * which could not be a write I think, it would have to be a read because of the write lock. Since it would be used essentially
+ * as not empty in the read case, then idk if I would really need 3 locks, since it would be impossible for all but one thread to work on the FIFO at any time.
+ * This may be good? But, the way I have it set up rn, I can have a read and write happening concurrently I think, which may be better, but seems slightly more expensive.
+ */
+
 int FIFO_push(FIFO_t *f, void *data) {
     int count;
 
