3b2b30196a695f0094cfb37db8f14f10fc552aac
Emmett-Kogan <emmett.kogan@gmail.com>
2024-02-17 18:31:30 -0500

    I think I'm done now. The program works for the given test case, I don't really feel like making a test case that is really good, so I'm going to wait for Yavuz to post hers. I don't think there will be any deadlock issues how my code is now, but I setup the test script to check for it.

--- Assignments/3/src/main.c
+++ Assignments/3/src/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #include "lib/FIFO.h"
 
-// Note depth isn't specified so I'm just defining this, could be depth of whatever tbh
 #define DEPTH 5
 #define MAX_STR_LEN 128
 
@@ -13,17 +14,17 @@ static void  *worker(void *args);
 
 typedef struct node_t {
     int id, balance;
+    sem_t s;
     struct node_t *next;
 } node_t;
 
 static node_t *head = NULL;
 static pthread_mutex_t action_lock;
-static pthread_mutex_t *account_locks;
 
 int main(int argc, char **argv) 
 {
     FILE *fptr = fopen(argv[1], "r");
-    char buffer[MAX_STR_LEN]; // lines could be really big, need to ask for some constraint from Yavuz
+    char buffer[MAX_STR_LEN];
 
     // Initialize account linked list
     int accounts = 0;
@@ -42,6 +43,7 @@ int main(int argc, char **argv)
         node_t *new_node = (node_t *) malloc(sizeof(node_t));
         new_node->id = id;
         new_node->balance = balance;
+        sem_init(&new_node->s, 0, 1);
         new_node->next = NULL;
 
         if (head) {
@@ -61,11 +63,8 @@ int main(int argc, char **argv)
     int num_threads = atoi(argv[2]);
     pthread_t *threads = malloc(num_threads*sizeof(pthread_t));
     FIFO_t *fifos = malloc(num_threads*sizeof(FIFO_t));
-
-    account_locks = malloc(accounts*sizeof(pthread_mutex_t));
+  
     pthread_mutex_init(&action_lock, NULL);
-    for (int i = 0; i < accounts; i++)
-        pthread_mutex_init(&account_locks[i], NULL);
 
     for (int i = 0; i < num_threads; i++) {
         FIFO_init(&fifos[i], DEPTH, MAX_STR_LEN);
@@ -117,28 +116,70 @@ static void *worker(void *args) {
         if (buffer[0] == '\n')
             break;
 
-        // Parse transfer arguements
+        // Parse transfer arguements and convert to something useful
         char *_from, *_to, *_amount, *tmp = buffer+9;
         _from = strtok_r(buffer+9, " ", &tmp);
         _to = strtok_r(0, " ", &tmp);
         _amount = strtok_r(0, " ", &tmp);
-
         int from = atoi(_from), to = atoi(_to), amount = atoi(_amount);
         
-        // Pick up action lock
-        // If both account locks are available, take them and release action lock
-            // Otherwise release all locks and try again after a sleep or something
-        
+        // Get pointers to both of the account nodes
+        node_t *acc_from = head;
+        while(acc_from) {
+            if (acc_from->id == from)
+                break;
+            acc_from = acc_from->next;
+        }
+
+        node_t *acc_to = head;
+        while(acc_to) {
+            if (acc_to->id == to)
+                break;
+            acc_to = acc_to->next;
+        }
+
+        // This basically polls the two required semaphores until they are available
+        int s_from = 0, s_to = 0;
+        while (1) {
+            // Get action lock
+            pthread_mutex_lock(&action_lock);
+
+            // Get values of both account semaphores
+            sem_getvalue(&acc_from->s, &s_from);
+            sem_getvalue(&acc_to->s, &s_to);
+
+            // If both available proceed
+            if (s_from == 1 && s_to == 1)
+                break;
+            
+            // Otherwise give up the action lock and block self
+            pthread_mutex_unlock(&action_lock);
+            usleep(1000);   // sleep for 1ms, BUGBUG
+        }
+
+        // Pick up both semaphores now that we know both are available
+        sem_wait(&acc_from->s);
+        sem_wait(&acc_to->s);
+
+        // Release action lock so while we are transferring someone else can try to do another
+        pthread_mutex_unlock(&action_lock);
+
         // Do transfer
+        acc_from->balance -= amount;
+        acc_to->balance   += amount;
 
         // Pick up action lock
-        // Once obtained release both account locks
+        pthread_mutex_lock(&action_lock);
 
+        // Once obtained release both accounts and action lock
+        sem_post(&acc_from->s);
+        sem_post(&acc_to->s);
+
+        pthread_mutex_unlock(&action_lock);
 
         // Reset buffer for next transfer
         memset(buffer, 0, MAX_STR_LEN);
     }
 
-    printf("Thread exiting!\n");
-    pthread_exit(0);
+    pthread_exit(NULL);
 }
---
+++ Assignments/3/src/test.sh
@@ -0,0 +1,20 @@
+make
+
+thread_counts=(1,2,4,8,16,32)
+for i in {0..0}; do
+    for j in ${thread_counts[@]}; do
+        timeout 20s ./a3 testfiles/input$i.txt $j >temp.txt
+        if [[ $? -eq 124 ]]; then
+            echo "Program timed out after 20 seconds during test $i with $j threads: possible deadlock"
+            continue
+        fi
+
+        diff temp.txt testfiles/output0.txt >/dev/null
+        if [[ $? -ne 0 ]]; then
+            echo "Differences found during test $i with $j threads"
+        fi
+    done
+done
+
+make clean
+rm temp.txt
