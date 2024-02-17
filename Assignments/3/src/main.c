0c93a703fd8b83f245e38eaf9301a0b4919c3268
Emmett-Kogan <emmett.kogan@gmail.com>
2024-02-17 16:59:48 -0500

    Updated assignment 3

--- Assignments/3/src/main.c
+++ Assignments/3/src/main.c
@@ -4,11 +4,12 @@
 #include <pthread.h>
 
 #include "lib/FIFO.h"
-#include "lib/threads.h"
 
 // Note depth isn't specified so I'm just defining this, could be depth of whatever tbh
 #define DEPTH 5
 #define MAX_STR_LEN 128
+
+static void  *worker(void *args);
 
 typedef struct node_t {
     int id, balance;
@@ -16,15 +17,19 @@ typedef struct node_t {
 } node_t;
 
 static node_t *head = NULL;
+static pthread_mutex_t action_lock;
+static pthread_mutex_t *account_locks;
 
 int main(int argc, char **argv) 
 {
     FILE *fptr = fopen(argv[1], "r");
-    char buffer[128]; // lines could be really big, need to ask for some constraint from Yavuz
+    char buffer[MAX_STR_LEN]; // lines could be really big, need to ask for some constraint from Yavuz
 
+    // Initialize account linked list
+    int accounts = 0;
     while (1) {
         // Get line of input
-        if (!fgets(buffer, 128, fptr) || buffer[0] == 'T')
+        if (!fgets(buffer, MAX_STR_LEN, fptr) || buffer[0] == 'T')
             break;
         
         // Parse line with strtok
@@ -39,53 +44,91 @@ int main(int argc, char **argv)
         new_node->balance = balance;
         new_node->next = head;
         head = new_node;
-
+        accounts++;
     }
 
-    // Want to print a traversal of the LL here once I'm done
-    printf("Traversal:\n");
-    node_t *node = head;
-    while(node) {
-        printf("ID: %d\tBalance: %d\n", node->id, node->balance);
-        node = node->next;
-    }
-
-    // Need to init threads here    
+    // Init threads and synchronization variables
     int num_threads = atoi(argv[2]);
-
     pthread_t *threads = malloc(num_threads*sizeof(pthread_t));
     FIFO_t *fifos = malloc(num_threads*sizeof(FIFO_t));
+
+    account_locks = malloc(accounts*sizeof(pthread_mutex_t));
+    pthread_mutex_init(&action_lock, NULL);
+    for (int i = 0; i < accounts; i++)
+        pthread_mutex_init(&account_locks[i], NULL);
 
     for (int i = 0; i < num_threads; i++) {
         FIFO_init(&fifos[i], DEPTH, MAX_STR_LEN);
         pthread_create(&threads[i], NULL, worker, (void *) &fifos[i]);
     }
 
-    // Step 2 is sending the tranfer strings to worker threads and having worker threads do something with them
-        // Want to do a do while since buffer already holds the first command
-        // Or maybe just a while where the condition is buffer[0] == 'T'
-
-    printf("\nTransfers:\n");
+    // Send transfers to threads in a round robin manner
+    int idx = 0;
     while(buffer[0] == 'T') {
-        printf("%s", buffer);
+        // Send string to a worker thread
+        FIFO_push(&fifos[idx], (void *) buffer);
+
+        // Increment idx
+        idx = (idx + 1) % num_threads; 
+
+        // Reset buffer and get next line
         memset(buffer, 0, 128);
         fgets(buffer, 128, fptr);
-
-        // Want to send each string to the worker thread for it to deal with
-
     }
-
-    // Step 3 is getting the worker threads to actually do their job
-
-
 
     // Cleanup
     void *res;
+    buffer[0] = '\n';
     for (int i = 0; i < num_threads; i++) {
+        FIFO_push(&fifos[i], buffer);
         pthread_join(threads[i], &res);
         FIFO_clean(&fifos[i]);
     }
 
     fclose(fptr);
+
+    // Print accounts while freeing linked list
+    node_t *tmp, *node = head;
+    while (node) {
+        printf("%d %d\n", node->id, node->balance);
+        tmp = node;
+        node = node->next;
+        free(tmp);
+    }
+
     return 0;
+}
+
+static void *worker(void *args) {
+    char buffer[MAX_STR_LEN];
+    while (1) {
+        FIFO_pop((FIFO_t *) args, buffer);
+
+        if (buffer[0] == '\n')
+            break;
+
+        // Parse transfer arguements
+        char *_from, *_to, *_amount, *tmp = buffer+9;
+        _from = strtok_r(buffer+9, " ", &tmp);
+        _to = strtok_r(0, " ", &tmp);
+        _amount = strtok_r(0, " ", &tmp);
+
+        int from = atoi(_from), to = atoi(_to), amount = atoi(_amount);
+        
+        // Pick up action lock
+        // If both account locks are available, take them and release action lock
+            // Otherwise release all locks and try again after a sleep or something
+        
+        // Do transfer
+
+        // Pick up action lock
+        // Once obtained release both account locks
+
+
+        // Reset buffer for next transfer
+        memset(buffer, 0, MAX_STR_LEN);
+    }
+
+    printf("Thread exiting!\n");
+    pthread_exit(0);
 }
