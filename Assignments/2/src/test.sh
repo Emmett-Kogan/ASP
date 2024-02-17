ca9c73d3f99a137236f68ccca263b1933fc3fcfd
Emmett-Kogan <emmett.kogan@gmail.com>
2024-02-17 18:53:53 -0500

    Improved test script

--- Assignments/2/src/test.sh
+++ Assignments/2/src/test.sh
@@ -3,13 +3,14 @@
 
 users=(3 5 10 10 20)
 depths=(1 2 3 5 10 20 $RANDOM $RANDOM $RANDOM $RANDOM $RANDOM)
+test_count=$(($(ls testfiles | wc -l)/2 -1))
 
 make
 
 # This tests each with a FIFO depth of 1, I also need to test for a bunch of different depths
 # Also, not in the spec is a number of FIFOs less than the number of users, but it doesn't specify that it can't be more. Mine handles this edgecase by just not using a bunch, but it may need to do something better for less
 
-for i in {0..4}; do
+for i in $(seq 0 $test_count); do
     for j in ${depths[@]}; do
 
         # want to shuffle the input file
