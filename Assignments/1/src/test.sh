2ada62d6e5dd1e4c48cf73124565820277eb957f
Emmett-Kogan <emmett.kogan@gmail.com>
2024-02-17 19:00:38 -0500

    Updated test script the same as 2 and 3. Also fixed the issue where it wasn't comparing to the rubric output after sorting the tested file's output

--- Assignments/1/src/test.sh
+++ Assignments/1/src/test.sh
@@ -1,10 +1,10 @@
 # this script assumes that the executable files are produced with a makefile and that they are named combiner reducer and mapper respectivley
 # this script also assumes you have a 'testfiles' directory that contains the files Yavuz distributed
 # note that test case 0 is technically broken because the ordering is different, Yavuz said that this is fine as long as the lines match (I didn't feel like sorting anything so I just left it as is)
-
+test_count=$(($(ls testfiles | wc -l)/2 -1))
 make
 
-for i in {0..4}; do
+for i in $(seq 0 $test_count); do
     ./mapper <testfiles/input$i.txt >temp.txt
     ./reducer <temp.txt >manual.txt
 
@@ -26,12 +26,13 @@ for i in {0..4}; do
         echo "Manual redirection output does not match combiner output"
     fi
 
-    diff mp.txt testfiles/output$i.txt
+    sort mp.txt > mp_s.txt
+    diff mp_s.txt testfiles/output$i.txt
     if [[ $? -ne 0 ]]
     then
         echo "Combiner output does not match given output"
     fi
 done
 
-rm mp.txt manual.txt temp.txt
+rm mp.txt mp_s.txt manual.txt temp.txt
 make clean
