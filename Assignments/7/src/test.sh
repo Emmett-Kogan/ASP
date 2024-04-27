c0a78c8f0bceea8c73bc5237f4e13cbab5aca2e1
emmett <emmett@linsus.myguest.virtualbox.org>
2024-04-26 20:59:02 -0400

    being safe

--- Assignments/7/src/test.sh
+++ Assignments/7/src/test.sh
@@ -4,10 +4,12 @@ sudo rmmod usbkbd
 sudo insmod usbkbd.ko
 
 # wait for user input to signify when hardware was recycled in virt-manager
-echo "Remove and add hardware in virt manager"
+echo "Remove and add hardware in virt box"
 read temp
+sleep 3s
+
 ls -l /sys/bus/usb/devices
 
 # bind device
-sudo echo -n "1-4:1.0" > /sys/bus/usb/drivers/usbhid/unbind
-sudo echo -n "1-4:1.0" > /sys/bus/usb/drivers/usbkbd/bind
+sudo echo -n "1-2:1.0" > /sys/bus/usb/drivers/usbhid/unbind
+sudo echo -n "1-2:1.0" > /sys/bus/usb/drivers/usbkbd/bind
